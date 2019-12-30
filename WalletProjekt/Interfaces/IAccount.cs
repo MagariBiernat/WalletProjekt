6d311dad26a945bdf4ba1054084fb63e03b58a91
MagariBiernat <123biernat123@gmail.com>
2019-12-30 22:23:22 +0100

    4

---
+++ WalletProjekt/Interfaces/IAccount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WalletProjekt
+{
+     interface IAccount
+    {
+        string _email { get; set; }
+        string _password { get; set; }
+        string _firstName { get; set; }
+        string _lastName { get; set; }
+        int _age { get; set; }
+        string _gender { get; set; }
+    }
+}
